85ff5b4bb9d212d00b734b7ed9fc425759c2c1a8
AidenShane <aidenshane472@gmail.com>
2026-07-24 10:50:07 -0400

    Add Police Tracker mod for crime tracking
    This mod tracks police-related events in the game, including player actions and crimes committed. It initializes with a key press and notifies the player of their wanted status.

---
+++ Police Tracker/mod.cs
@@ -0,0 +1,306 @@
+using GTA;
+using GTA.Math;
+using GTA.Native;
+using GTA.UI;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace GTAPoliceTrackerMod;
+
+public class PoliceTracker : Script
+{
+    private Keys _initKey = Keys.F6;
+
+    bool _initialized = false;
+    bool _useAbbreviations = false;
+    string _lastCrime = "";
+    Player _player = Game.Player;
+    private string _pendingCrime = "";
+
+    private int _lastWantedLevel = 0;
+    private int _playerKillCount = 0;
+
+    private readonly HashSet<int> _deadPeds = new();
+
+    public event EventHandler<CrimeChangedEventArgs> CrimeChanged;
+
+    public PoliceTracker()
+    {
+        KeyDown += OnKeyDown;
+        Notify($"Police tracker loaded, press {_initKey} to toggle the mod!");
+    }
+
+    private void Initialize()
+    {
+        if (_initialized)
+        {
+            Console.WriteLine("Attempt to run PoliceTracker.Initialize but mod is initialized!");
+            return;
+        }
+
+        _initialized = true;
+
+        CrimeChanged += OnCrimeChanged;
+        Tick += OnTick;
+
+        Notify("~g~Initialized Police Tracker!");
+    }
+
+    private void Deinitialize()
+    {
+        if (!_initialized)
+        {
+            Console.WriteLine("Attempt to run PoliceTracker.Deinitialize but mod isn't initialized!");
+            return;
+        }
+
+        _initialized = false;
+
+        CrimeChanged += null;
+        Tick += null;
+
+        Notify("~r~Deinitialized Police Tracker!");
+    }
+
+    private void OnKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.KeyCode == _initKey)
+        {
+            InitKeyPressed();
+        }
+        else
+        {
+            ActionKeyPressed(e.KeyCode);
+        }
+    }
+
+    private void ActionKeyPressed(Keys key)
+    {
+
+    }
+
+    private void InitKeyPressed()
+    {
+        if (_initialized)
+        {
+            Deinitialize();
+        }
+        else
+        {
+            Initialize();
+        }
+    }
+
+    private void OnTick(object sender, EventArgs e)
+    {
+        int wantedLevel = _player.Wanted.WantedLevel;
+
+        if (wantedLevel > 0)
+        {
+            CrimeChanged += OnCrimeChanged;
+        }
+        else
+        {
+            CrimeChanged += null;
+            _lastCrime = "";
+            _playerKillCount = 0;
+        }
+
+        foreach (Ped ped in World.GetNearbyPeds(Game.Player.Character, 100f))
+        {
+            if (ped.IsDead && _deadPeds.Add(ped.Handle))
+            {
+                OnPedDeath(ped);
+            }
+            else
+            {
+                OnPedCrime(ped);
+            }
+        }
+
+        if (wantedLevel <= 0)
+        {
+            SetCrime("");
+            _pendingCrime = "";
+            _playerKillCount = 0;
+        }
+
+        if (wantedLevel > _lastWantedLevel && _pendingCrime != "")
+        {
+            SetCrime(_pendingCrime);
+            _pendingCrime = "";
+        }
+
+        _lastWantedLevel = wantedLevel;
+    }
+
+    private void OnPedDeath(Ped ped)
+    {
+        Entity killer = ped.Killer;
+        
+
+        if (killer == null)
+            return;
+
+        bool playerKilled =
+            killer.Handle == _player.Character.Handle ||
+            (_player.Character.IsInVehicle() &&
+             killer.Handle == _player.Character.CurrentVehicle.Handle);
+
+        if (!playerKilled)
+            return;
+
+        _playerKillCount++;
+
+        string crime;
+
+        if (_player.Character.IsInVehicle() && !ped.HasBeenDamagedByAnyWeapon())
+        {
+            crime = "Vehicular Manslaughter";
+        }
+        else if (_playerKillCount >= 2)
+        {
+            crime = "Homicide";
+        }
+        else
+        {
+            crime = "Murder";
+        }
+
+        if (ped.PedType == PedType.Cop)
+        {
+            crime = $"~r~{crime} Of a Police Officer~s~";
+        }
+
+        _pendingCrime = crime;
+    }
+
+    private void OnPedCrime(Ped ped)
+    {
+        if (ped.IsDead)
+            return;
+
+        if (ped.IsOnFire)
+        {
+            string crime = "Arson";
+
+            if (ped.PedType == PedType.Cop)
+            {
+                crime = $"~r~{crime} Of a Police Officer~s~";
+            }
+
+            SetCrime(crime);
+            return;
+        }
+
+        if (ped.HasBeenDamagedBy(_player.Character) && !ped.IsDead)
+        {
+            string crime = "Assault";
+
+            if (ped.PedType == PedType.Cop)
+            {
+                crime = $"~r~{crime} Of a Police Officer~s~";
+            }
+
+            SetCrime(crime);
+        }
+    }
+    private void SetCrime(string crime)
+    {
+        if (_lastCrime == crime) return;
+
+        CrimeChanged?.Invoke(this, new CrimeChangedEventArgs(crime, _lastCrime));
+
+        _lastCrime = crime;
+        Console.WriteLine($"Setting Last crime to {crime}");
+    }
+
+    private void OnCrimeChanged(object sender, CrimeChangedEventArgs e)
+    {
+        if (e.OldCrime == "")
+        {
+            Notify($"You're now wanted by ~b~{GetAgency()}~s~ for ~r~{e.NewCrime}~s~");
+        }
+        else
+        {
+            Notify($"You're now wanted for ~r~{e.NewCrime}~s~");
+        }
+    }
+
+    private string GetAgency()
+    {
+        string region = GetRegion();
+
+        if (_useAbbreviations)
+        {
+            return region switch
+            {
+                "Los Santos" => "LSPD",
+                "Blaine County" => "BCSO",
+                _ => "SAHP"
+            };
+        }
+        else
+        {
+            return region switch
+            {
+                "Los Santos" => "Los Santos Police Department",
+                "Blaine County" => "Blaine County Sheriff's Office",
+                _ => "San Andreas Highway Patrol"
+            };
+        }
+    }
+
+    private void Notify(string text, bool important = false)
+    {
+        Notification.PostTicker(text, important);
+    }
+
+    private string GetRegion()
+    {
+        Vector3 pos = _player.Character.Position;
+
+        string zone = Function.Call<string>(
+            Hash.GET_NAME_OF_ZONE,
+            pos.X,
+            pos.Y,
+            pos.Z
+        );
+
+        return zone switch
+        {
+            // los santos
+            "AIRP" or "ALTA" or "BANNING" or "BEACH" or "BURTON" or
+            "CHAMH" or "CYPRE" or "DAVIS" or "DELBE" or "DELPE" or
+            "DOWNTOWN" or "DTVINE" or "EAST_V" or "ELYSIAN" or
+            "HAWICK" or "KOREAT" or "LEGSQU" or "LMESA" or
+            "LOSPUER" or "MIRR" or "MORN" or "PBOX" or
+            "RANCHO" or "RICHM" or "ROCKF" or "SKID" or
+            "STAD" or "STRAW" or "TEXTI" or "VCANA" or
+            "VESP" or "VINE" or "WVINE"
+                => "Los Santos",
+
+            // blaine county
+            "ALAMO" or "CHIL" or "CHU" or "DESRT" or "GREATC" or
+            "GRAPES" or "HARMO" or "JAIL" or "MTCHIL" or
+            "MTGORDO" or "PALETO" or "PALFOR" or "SANDY" or
+            "SLAB" or "TATAMO" or "TONGVA" or "WINDF" or
+            "ZANCUDO"
+                => "Blaine County",
+
+            _ => "Unknown"
+        };
+    }
+}
+
+public class CrimeChangedEventArgs
+{
+    public string NewCrime { get; private set; }
+    public string OldCrime { get; private set; }
+
+    public CrimeChangedEventArgs(string _new, string _last)
+    {
+        NewCrime = _new;
+        OldCrime = _last;
+    }
+}
